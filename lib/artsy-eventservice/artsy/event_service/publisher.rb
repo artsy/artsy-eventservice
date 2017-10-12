70f06795cfe009cbfa8ac220ce55dda9810ec233
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-10-12 14:18:19 +0200

    Refactor RabbitMQConnection to a class - initialize connection as a class variable and synchronize access to channels created via get_channel method

--- lib/artsy-eventservice/artsy/event_service/publisher.rb
+++ lib/artsy-eventservice/artsy/event_service/publisher.rb
@@ -2,16 +2,13 @@
 module Artsy
   module EventService
     class Publisher
-      include RabbitMQConnection
-
       def self.publish(topic:, event:, routing_key: nil)
         new.post_event(topic: topic, event: event, routing_key: routing_key)
       end
 
       def post_event(topic:, event:, routing_key: nil)
         raise 'Event missing topic or verb.' if event.verb.to_s.empty? || topic.to_s.empty?
-        connect_to_rabbit do |conn|
-          channel = conn.create_channel
+        RabbitMQConnection.get_channel do |channel|
           channel.confirm_select if config.confirms_enabled
           exchange = channel.topic(topic, durable: true)
           exchange.publish(
