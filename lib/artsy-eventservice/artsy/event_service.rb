b0f3de86da0b841b65985691bd5cdcd4610a5398
Stephen Olsen <steve.olsen@gmail.com>
2018-01-15 16:18:24 -0500

    add endpoint for arbitrary data

--- lib/artsy-eventservice/artsy/event_service.rb
+++ lib/artsy-eventservice/artsy/event_service.rb
@@ -2,6 +2,23 @@
 # frozen_string_literal: true
 module Artsy
   module EventService
+
+    # Post data without an event, must be serialized.
+    def self.post_data(topic:, data:, routing_key:)
+      RabbitMQConnection.get_channel do |channel|
+        channel.confirm_select if Artsy::EventService.config.confirms_enabled
+        exchange = channel.topic(topic, durable: true)
+        exchange.publish(
+          data,
+          routing_key: routing_key,
+          persistent: true,
+          content_type: 'application/json',
+          app_id: Artsy::EventService.config.app_name
+        )
+        raise 'Publishing data failed' if Artsy::EventService.config.confirms_enabled && !channel.wait_for_confirms
+      end
+    end
+
     def self.post_event(topic:, event:, routing_key: nil)
       return unless event_stream_enabled?
       Publisher.publish(topic: topic, event: event, routing_key: routing_key || event.routing_key)
